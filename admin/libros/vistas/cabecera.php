e39d8bbee8ce026dcfca2920034f730df6afb8ea
JeroFD <94115243+JeroFD@users.noreply.github.com>
2022-02-09 19:59:17 +0000

    Delete admin directory

--- admin/libros/vistas/cabecera.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Libros</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/fe280c8931.js" crossorigin="anonymous"></script>
-</head>
-<div class="container">
